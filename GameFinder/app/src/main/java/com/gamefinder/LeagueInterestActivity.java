e504788aab8b0bfd07d46269639f3434017012df
Paul Kim <skim668@gatech.edu>
2016-03-06 15:18:31 -0500

    Added UI for displaying leagues
    Added UI elements and working towards rating system

--- GameFinder/app/src/main/java/com/gamefinder/LeagueInterestActivity.java
+++ GameFinder/app/src/main/java/com/gamefinder/LeagueInterestActivity.java
@@ -1,16 +1,23 @@
 package com.gamefinder;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import okhttp3.Interceptor;
@@ -26,14 +33,15 @@ public class LeagueInterestActivity extends AppCompatActivity {
     public final String BASE_URL = "https://fathomless-woodland-78351.herokuapp.com/api/";
     private ListView listView;
     ArrayAdapter<String> adapter;
-    String[] leagues;
-    int[] ids;
+    ArrayList<String> leagues;
+    ArrayList<Integer> ids;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_league_interest);
 
+        listView = (ListView) findViewById(R.id.listView);
         Intent intent = getIntent();
         String accessToken = intent.getStringExtra("accessToken");
         String client = intent.getStringExtra("client");
@@ -46,12 +54,34 @@ public class LeagueInterestActivity extends AppCompatActivity {
 
         final APIService service = retrofit.create(APIService.class);
 
+        leagues = new ArrayList<String>();
+        ids = new ArrayList<Integer>();
+
         Call<List<LeaguesResponse>> call = service.getLeagues(accessToken, client, uid);
         call.enqueue(new Callback<List<LeaguesResponse>>() {
             @Override
             public void onResponse(Call<List<LeaguesResponse>> call, retrofit2.Response<List<LeaguesResponse>> response) {
                 if (response.isSuccess()) {
                     List<LeaguesResponse> responseBody = response.body();
+                    for (int i = 0; i < responseBody.size(); i++) {
+                        leagues.add(responseBody.get(i).getName());
+                        ids.add(Integer.parseInt(responseBody.get(i).getId())); //ids not used yet
+                    }
+                    final StableArrayAdapter adapter = new StableArrayAdapter(getApplicationContext(),
+                            android.R.layout.simple_list_item_1, leagues) {
+                        @Override
+                        public View getView(int position, View convertView,
+                                            ViewGroup parent) {
+                            View view =super.getView(position, convertView, parent);
+
+                            TextView textView=(TextView) view.findViewById(android.R.id.text1);
+
+                            /*YOUR CHOICE OF COLOR*/
+                            textView.setTextColor(Color.BLACK);
+                            return view;
+                        }
+                    };
+                    listView.setAdapter(adapter);
                     System.out.println(responseBody.toString());
                 } else {
                     System.out.println("response failure");
@@ -64,6 +94,36 @@ public class LeagueInterestActivity extends AppCompatActivity {
             }
         });
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view,
+                                    int position, long id) {
+                final String item = (String) parent.getItemAtPosition(position);
+                view.animate().setDuration(2000).alpha(0)
+                        .withEndAction(new Runnable() {
+                            @Override
+                            public void run() {
+                                leagues.remove(item);
+                                adapter.notifyDataSetChanged();
+                                view.setAlpha(1);
+                            }
+                        });
+            }
+
+        });
     }
 
+    private class StableArrayAdapter extends ArrayAdapter<String> {
+
+        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+        public StableArrayAdapter(Context context, int textViewResourceId,
+                                  List<String> objects) {
+            super(context, textViewResourceId, objects);
+            for (int i = 0; i < objects.size(); ++i) {
+                mIdMap.put(objects.get(i), i);
+            }
+        }
+    }
 }
---
+++ GameFinder/app/src/main/java/com/gamefinder/TeamInterestActivity.java
@@ -0,0 +1,13 @@
+package com.gamefinder;
+
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+/**
+ * Created by Paul on 3/6/2016.
+ */
+public class TeamInterestActivity {
+    public final String BASE_URL = "https://fathomless-woodland-78351.herokuapp.com/api/";
+    private ListView listView;
+    ArrayAdapter<String> adapter;
+}
