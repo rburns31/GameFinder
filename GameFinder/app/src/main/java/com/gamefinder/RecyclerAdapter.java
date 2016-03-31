2c17747e591568a8689071c2c950039fb7db49cf
Paul Kim <skim668@gatech.edu>
2016-03-31 09:55:46 -0400

    Small change to display
    Changed to network from score

--- GameFinder/app/src/main/java/com/gamefinder/RecyclerAdapter.java
+++ GameFinder/app/src/main/java/com/gamefinder/RecyclerAdapter.java
@@ -33,7 +33,7 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
         holder.gameName.setText(gamesList.get(position).getCompetitor_1().getName() + " vs " + gamesList.get(position).getCompetitor_2().getName());
-        holder.gameDescription.setText("Start Time: " + gamesList.get(position).getStart_time() + " Score: " + gamesList.get(position).getScore());
+        holder.gameDescription.setText("Start Time: " + gamesList.get(position).getStart_time() + " on " + gamesList.get(position).getNetwork());
     }
 
     @Override
